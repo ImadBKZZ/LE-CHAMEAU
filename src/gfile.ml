e9d6f330bf64ca0033db9df872ff5cacc22b828c
AurelienPasquet <apasquet@insa-toulouse.fr>
2023-11-27 11:14:04 +0100

    export complétée

--- src/gfile.ml
+++ src/gfile.ml
@@ -37,6 +37,11 @@ let compute_y id =
     let ff = open_out path in
     
     fprintf ff "digraph finite_state_machine {\n";
+    fprintf ff "  fontname=\"Helvetica,Arial,sans-serif\"\n";
+    fprintf ff "  node [fontname=\"Helvetica,Arial,sans-serif\"]\n";
+    fprintf ff "  edge [fontname=\"Helvetica,Arial,sans-serif\"]\n";
+    fprintf ff "  rankdir=LR;\n";
+    fprintf ff "  node [shape = circle];";
 
     (* Write all arcs *)
     let _ = e_fold graph (fun count arc -> fprintf ff "  %d -> %d [label = %s];\n" arc.src arc.tgt arc.lbl; count + 1) 0 in
